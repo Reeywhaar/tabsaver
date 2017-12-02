e4eaa3865f4ac8a253e54d0aafd9e0e9c51c8b8e
Mikhail Vyrtsev <reeywhaar@gmail.com>
2017-12-02 23:32:00 +0300

    battling with webpack dilemmas

--- webpack.config.js
+++ webpack.config.js
@@ -6,61 +6,68 @@ const extractSVG = new ExtractTextPlugin("[name]");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 
-module.exports = [
-	{
-		entry: {
-			panel: './src/panel.js',
-			background: "./src/background.js",
-			handler: "./src/handler.js",
-		},
-		output: {
-			path: path.resolve(__dirname, 'ext/js'),
-			filename: '[name].js'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.vue$/,
-					use: "vue-loader"
-				},
+module.exports = (h, args) => {
+	const additionalPlugins = [];
+	if(!args.watch){
+		additionalPlugins.push(new MinifyPlugin());
+	};
+
+	return [
+		{
+			entry: {
+				panel: './src/panel.js',
+				background: "./src/background.js",
+				handler: "./src/handler.js",
+			},
+			output: {
+				path: path.resolve(__dirname, 'ext/js'),
+				filename: '[name].js'
+			},
+			module: {
+				rules: [
+					{
+						test: /\.vue$/,
+						use: "vue-loader"
+					},
+				]
+			},
+			plugins: [
+				new webpack.optimize.ModuleConcatenationPlugin(),
+				new webpack.DefinePlugin({
+					'process.env': {
+						NODE_ENV: '"production"'
+					}
+				}),
+				...additionalPlugins,
 			]
 		},
-		plugins: [
-			new webpack.optimize.ModuleConcatenationPlugin(),
-			new webpack.DefinePlugin({
-				'process.env': {
-					NODE_ENV: '"production"'
+		{
+			entry: {
+				"icon.svg": "./icon.svg.js",
+			},
+			output: {
+				path: path.resolve(__dirname, 'ext/icons'),
+				filename: '[name]'
+			},
+			resolveLoader: {
+				alias: {
+					'svg-js-loader': '@reeywhaar/svgmaker/loader.js',
 				}
-			}),
-			new MinifyPlugin(),
-		]
-	},
-	{
-		entry: {
-			"icon.svg": "./icon.svg.js",
-		},
-		output: {
-			path: path.resolve(__dirname, 'ext/icons'),
-			filename: '[name]'
-		},
-		resolveLoader: {
-			alias: {
-				'svg-js-loader': '@reeywhaar/svgmaker/loader.js',
-			}
-		},
-		module: {
-			rules: [
-				{
-					test: /\.svg\.js$/,
-					use: extractSVG.extract({
-						use: "svg-js-loader"
-					}),
-				},
+			},
+			module: {
+				rules: [
+					{
+						test: /\.svg\.js$/,
+						use: extractSVG.extract({
+							use: "svg-js-loader"
+						}),
+					},
+				]
+			},
+			plugins: [
+				extractSVG,
+				new InertEntryPlugin(),
 			]
-		},
-		plugins: [
-			extractSVG,
-			new InertEntryPlugin(),
-		]
-	}
-];
+		}
+	];
+};
